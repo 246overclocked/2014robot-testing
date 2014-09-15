60e6c683db6d3511ea69a26a6e2933aa1a66a65a
michaelsilver <yomichael88@yahoo.com>
2014-09-15 17:50:07 -0400

    pre-merge changes

--- 2014robot/src/org/usfirst/frc246/2014robot/subsystems/Shooter.java
+++ 2014robot/src/org/usfirst/frc246/2014robot/subsystems/Shooter.java
@@ -43,5 +43,10 @@ public class Shooter extends Subsystem {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    public void prepareToShoot() {
+        
+        
+    }
 }
 
