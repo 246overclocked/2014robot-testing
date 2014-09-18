9b94af68177939c318843d7ccaf19af6a14c0898
Owen-Gillespie <phin1797@gmail.com>
2014-09-18 15:56:38 -0400

    Fixed some stuff with stopping motors

--- 2014robot/src/org/usfirst/frc246/commands/ShooterOff.java
+++ 2014robot/src/org/usfirst/frc246/commands/ShooterOff.java
@@ -45,6 +45,7 @@ public class  ShooterOff extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        Robot.shooter.stopMotors();
     }
 
     // Called when another command which requires one or more of the same
